2e9bc3cf1ca12794c0303c331ac4afafbd36fd78
Janus Nicon <janusnic@gmail.com>
2017-08-15 16:14:00 +0300

    added unit7

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,7 +1,7 @@
-let Posts = require('../models/post');
+const models = require("../models");
 
 exports.index = (req, res) => {
-    Posts.all((err, docs) => {
+    models.Post.all((err, docs) => {
         res.render('index', { 
             title: 'Post List',
             posts: docs 
@@ -10,10 +10,53 @@ exports.index = (req, res) => {
 };
 
 exports.show = (req, res, next) => {
-    Posts.detail(req.params.id, (err, docs) => {
+    models.Post.detail(req.params.id, (err, docs) => {
         res.render('post', { 
             title: 'Post Detail',
             post: docs 
         });
     });
 };
+
+
+// const URLSlugs = require('mongoose-url-slugs');
+router.get('/post', function(req,res,next)
+{
+    res.render('addpost',{
+    });
+});
+
+router.post('/post', function(req, res, next)
+{
+    let post = new models.Post(req.body);
+    post.save()
+        .then(()=>{
+            res.redirect('/post/' + post._id);
+        }).catch(next);
+});
+
+router.get('/post/:id',(req, res, next)=>{
+    models.Post.findOne({
+        _id:req.params.id
+    }).exec().then((post)=>{
+        if(!post) res.redirect('/#notfound');
+        res.render('post',{
+            post
+        });
+    }).catch(next);
+});
+
+router.get('/post/:slug',(req, res, next)=>{
+    models.Post.findOne({
+        slug:req.params.slug
+    }).exec().then((post)=>{
+        if(!post) res.redirect('/#notfound');
+        res.render('post',{
+            user:req.user,
+            post
+        });
+    }).catch(next);
+});
+
+// Подключим генератор на основе названия
+//postSchema.plugin(URLSlugs('title'));
